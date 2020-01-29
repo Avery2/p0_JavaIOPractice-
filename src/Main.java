50a9f3f11034bb5d099d282a7c7df2e6a9b25803
Avery2 <JustinAveryChan@gmail.com>
2020-01-28 20:33:16 -0600

    make room work

--- src/Main.java
+++ src/Main.java
@@ -16,30 +16,19 @@ import java.util.Scanner;
  *
  */
 public class Main {
-  private static Scanner keyboard;
+  private static Scanner sc;
   private static Scanner text;
-  // ANSI escape sequences to color text
-  private static final String BLACK = "\u001B[30m";
-  private static final String RED = "\u001B[31m";
-  private static final String YELLOW = "\u001B[33m";
-  private static final String BLUE = "\u001B[34m";
-  private static final String PURPLE = "\u001B[35m";
-  private static final String CYAN = "\u001B[36m";
-  private static final String[] COLORS = {BLACK, RED, YELLOW, BLUE, PURPLE, CYAN};
-  private static final String[] COLORNAMES = {"black", "red", "yellow", "blue", "purple", "cyan"};
-  private static final String[] OSNAMES = {"MacOS", "Windows", "Linux"};
-  private static final String RESET = "\u001B[0m"; // reset to default text color
 
-  private static String userName, customOS = "";
-  private static int OS, favColor;
-  private static boolean usesLinux;
+  public enum direction {
+    UP, DOWN, LEFT, RIGHT
+  }
 
   /**
    * Initializes resources.
    */
   public static void initialize() {
     // initialize
-    keyboard = new Scanner(System.in);
+    sc = new Scanner(System.in);
     try {
       text = new Scanner(new File("stuff.txt"));
     } catch (FileNotFoundException f) {
@@ -48,76 +37,163 @@ public class Main {
     // System.out.println("Text file says: " + text.nextLine());
   }
 
+  public static void pr() {
+    System.out.println("Hi");
+  }
+
   /**
    * Closes resources.
    */
   public static void terminate() {
     // close streams
-    keyboard.close();
-    text.close();
+    sc.close();
+    if (text != null) {
+      text.close();
+    }
   }
 
   /**
-   * Prompt user and records their responses.
+   * Asks
+   * 
+   * @param firstAsk
+   * @return An movement direction.
    */
-  public static void prompt() {
-    // prompt user
-    System.out.println("Tell me about yourself...");
-
-    System.out.println("What's your name?");
-    System.out.print("> ");
-    userName = keyboard.nextLine();
-
-    System.out.println("What operating system do you use?\n" + "[1] MacOS\n" + "[2] Windows\n"
-        + "[3] Linux\n" + "[4] Other");
-    System.out.print("> ");
-    OS = keyboard.nextInt() - 1;
-    keyboard.nextLine();
-    if (OS == 3) {
-      System.out.println("What OS do you use?");
-      System.out.print("> ");
-      customOS = keyboard.nextLine();
+  public static direction getMovement(Room rm, boolean firstAsk) {
+    if (firstAsk) {
+      System.out.println("Which direction?");
+      if (rm.hasUp()) {
+        System.out.println("  [w] up");
+      } else {
+        System.out.println("  [ ]");
+      }
+      if (rm.hasDown()) {
+        System.out.println("  [s] down");
+      } else {
+        System.out.println("  [ ]");
+      }
+      if (rm.hasLeft()) {
+        System.out.println("  [a] left");
+      } else {
+        System.out.println("  [ ]");
+      }
+      if (rm.hasRight()) {
+        System.out.println("  [d] right");
+      } else {
+        System.out.println("  [ ]");
+      }
     }
-    System.out.println("Are you using a linux console?\n" + "[1] Yes\n" + "[2] No");
     System.out.print("> ");
-    usesLinux = keyboard.nextInt() == 1;
-
-    System.out.println("What's your favorite color?\n" + "[1] Black\n" + "[2] Red\n"
-        + "[3] Yellow\n" + "[4] Blue\n" + "[5] Purple\n" + "[6] Cyan");
-    System.out.print("> ");
-    favColor = keyboard.nextInt() - 1;
-    keyboard.nextLine();
+    char response = sc.nextLine().charAt(0);
+    switch (response) {
+      case 'w':
+        if (rm.hasUp()) {
+          return direction.UP;
+        } else {
+          System.out.println("No room above.");
+          return getMovement(rm, false);
+        }
+      case 's':
+        if (rm.hasDown()) {
+          return direction.DOWN;
+        } else {
+          System.out.println("No room below.");
+          return getMovement(rm, false);
+        }
+      case 'a':
+        if (rm.hasLeft()) {
+          return direction.LEFT;
+        } else {
+          System.out.println("No room to the left.");
+          return getMovement(rm, false);
+        }
+      case 'd':
+        if (rm.hasRight()) {
+          return direction.RIGHT;
+        } else {
+          System.out.println("No room to the right.");
+          return getMovement(rm, false);
+        }
+      default:
+        System.out.println("Invalid input.");
+        return getMovement(rm, false);
+    }
   }
 
-  /**
-   * Responds to user by outputting information about them.
-   */
-  public static void respond() {
-    // respond to user based on input
-    System.out.println("\nHere are some facts about you...");
-    if (OS != 3) {
-      System.out.println("You are using " + OSNAMES[OS] + ".");
+  public static void printRoom(Room rm) {
+    String tag;
+    if (rm.isGoal()) {
+      tag = "X";
     } else {
-      System.out.println("You are using " + customOS + ".");
+      tag = "" + rm.getId();
     }
-    if (usesLinux) { // ANSI escape sequences only reliably works on linux consoles
-      System.out.print("Your favorite color is ");
-      System.out.println(COLORS[favColor] + COLORNAMES[favColor] + RESET + ".");
+    System.out.print("\n  ┌─");
+    // up?
+    if (rm.hasUp()) {
+      System.out.print("┴");
     } else {
-      System.out.println(COLORNAMES[favColor] + ".");
+      System.out.print("─");
     }
-    System.out.println("That's it, bye " + userName + "!");
+    System.out.print("─┐\n");
+    System.out.print(" ");
+    // left?
+    if (rm.hasLeft()) {
+      System.out.print("─┤");
+    } else {
+      System.out.print(" │");
+    }
+    System.out.print(" " + tag + " ");
+    // right?
+    if (rm.hasRight()) {
+      System.out.print("├─");
+    } else {
+      System.out.print("│");
+    }
+    System.out.print("\n");
+    System.out.print("  └─");
+    // down?
+    if (rm.hasDown()) {
+      System.out.print("┬");
+    } else {
+      System.out.print("─");
+    }
+    System.out.print("─┘\n\n");
   }
 
   public static void main(String[] args) {
-    System.out.println("┐");
-    System.out.println((int) '┐');
-    // for (int i = 0; i < 300; i++) {
-    // System.out.println(i + ": " + (char) i);
-    // }
-    // initialize();
-    // prompt();
-    // respond();
-    // terminate();
+    initialize();
+    Room center = new Room();
+    center.generateRoom(direction.UP).generateRoom(direction.UP);
+    center.generateRoom(direction.DOWN).generateRoom(direction.DOWN);
+    center.generateRoom(direction.LEFT).generateRoom(direction.LEFT);
+    center.generateRoom(direction.RIGHT).generateRoom(direction.RIGHT).setGoal(true);
+    Room currRoom = center;
+
+    // currRoom.getLeft().setGoal(true);
+    direction move;
+
+    while (true) {
+      printRoom(currRoom);
+      if (currRoom.isGoal()) {
+        System.out.println("You win");
+        break;
+      }
+      move = getMovement(currRoom, true);
+      switch (move) {
+        case UP:
+          currRoom = currRoom.getUp();
+          break;
+        case DOWN:
+          currRoom = currRoom.getDown();
+          break;
+        case LEFT:
+          currRoom = currRoom.getLeft();
+          break;
+        case RIGHT:
+          currRoom = currRoom.getRight();
+          break;
+      }
+      System.out.println("===============");
+    }
+    terminate();
   }
 }
